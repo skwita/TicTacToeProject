9c7273aa2272f96e2b3efe5620c561eed246b231
skwita <skwita.max@gmail.com>
2020-02-19 17:09:32 +0300

    added methods "PrintCanvas", "SetCross", "SetNought" (temp main is for tests only)

--- src/project/TicTacToe.java
+++ src/project/TicTacToe.java
@@ -1,7 +1,50 @@
 package project;
 
 public class TicTacToe {
-    public static void main(String[] args) {
-        System.out.println("lol");
+    private int size;
+    private String[][] board;
+
+    private TicTacToe(int size) {
+        this.board = new String[size][size];
+        this.size = size;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = "_";
+            }
+        }
     }
+
+
+
+    private static void PrintCanvas(TicTacToe test) {
+        for (int i = 0; i < test.size; i++) {
+            for (int j = 0; j < test.size; j++) {
+                System.out.print(test.board[i][j] + "  ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    private static void SetCross(int x, int y, TicTacToe test) {
+        if (test.board[x-1][y-1].equals("_") && x < test.size && y < test.size) {
+            test.board[x-1][y-1] = "X";
+        }
+    }
+
+    private static void SetNought (int x, int y, TicTacToe test) {
+        if (test.board[x-1][y-1].equals("_") && x < test.size && y < test.size) {
+            test.board[x-1][y-1] = "O";
+        }
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe(10);
+        PrintCanvas(game);
+        System.out.println();
+        SetCross(1,2, game);
+        SetNought(4, 3, game);
+        PrintCanvas(game);
+    }
+
+
 }
