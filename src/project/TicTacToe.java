86506831c020e4ae74c9681c9126f8ae69164b6a
skwita <skwita.max@gmail.com>
2020-02-20 17:55:51 +0300

    fixed methods "clear","setCross","steNought" fixed code style

--- src/project/TicTacToe.java
+++ src/project/TicTacToe.java
@@ -1,50 +1,54 @@
 package project;
 
-public class TicTacToe {
+class TicTacToe {
     private int size;
-    private String[][] board;
+    String[][] board;
 
-    private TicTacToe(int size) {
+    TicTacToe(int size) {
         this.board = new String[size][size];
         this.size = size;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                board[i][j] = "_";
+                board[j][i] = "_";
             }
         }
     }
 
-
-
-    private static void PrintCanvas(TicTacToe test) {
+    private static void printCanvas(TicTacToe test) {
         for (int i = 0; i < test.size; i++) {
             for (int j = 0; j < test.size; j++) {
-                System.out.print(test.board[i][j] + "  ");
+                System.out.print(test.board[j][i] + "  ");
             }
             System.out.print("\n");
         }
     }
 
-    private static void SetCross(int x, int y, TicTacToe test) {
-        if (test.board[x-1][y-1].equals("_") && x < test.size && y < test.size) {
+    static void setCross(int x, int y, TicTacToe test) {
+        if (test.board[x-1][y-1].equals("_")) {
             test.board[x-1][y-1] = "X";
         }
     }
 
-    private static void SetNought (int x, int y, TicTacToe test) {
-        if (test.board[x-1][y-1].equals("_") && x < test.size && y < test.size) {
+    static void setNought (int x, int y, TicTacToe test) {
+        if (test.board[x-1][y-1].equals("_")) {
             test.board[x-1][y-1] = "O";
         }
     }
 
-    public static void main(String[] args) {
-        TicTacToe game = new TicTacToe(10);
-        PrintCanvas(game);
-        System.out.println();
-        SetCross(1,2, game);
-        SetNought(4, 3, game);
-        PrintCanvas(game);
+    static void clear (int x, int y, TicTacToe test) {
+            test.board[x-1][y-1] = "_";
     }
 
+    private static int[][] maxLengthCrossCheck(TicTacToe test){
+        int[] firstCurrentPos = new int[] {0, 0};
+        int[] secondCurrentPos = new int[] {0, 0};
+        int[] firstMaxPos = new int[] {0, 0};
+        int[] secondMaxPos = new int[] {0, 0};
+        for (int i = 0; i < test.size; i++) {
+            for (int j = 0; j < test.size; j++) {
 
+            }
+        }
+        return new int[][] {firstMaxPos, secondMaxPos};
+    }
 }
