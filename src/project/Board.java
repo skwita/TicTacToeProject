61cf131d0d84937a52ea0904a8d1887e2360d704
skwita <skwita.max@gmail.com>
2020-05-25 11:44:25 +0300

    added

---
+++ src/project/Board.java
@@ -0,0 +1,7 @@
+package project;
+
+public enum Board {
+    X,
+    O,
+    E
+}
---
+++ src/project/TwoPositions.java
@@ -0,0 +1,30 @@
+package project;
+
+public class TwoPositions {
+    private int startFirst;
+    private int startSecond;
+    private int endFirst;
+    private int endSecond;
+
+    public void addStart(int i, int j){
+        startFirst = i;
+        startSecond = j;
+    }
+    public void addEnd(int i, int j){
+        endFirst = i;
+        endSecond = j;
+    }
+    public void clear(){
+        startFirst = -1;
+        startSecond = -1;
+        endFirst = -1;
+        endSecond = -1;
+    }
+    public int length(boolean vertical){
+        if (vertical){
+            return (endFirst - startFirst + 1);
+        } else {
+            return (endSecond - startSecond + 1);
+        }
+    }
+}
